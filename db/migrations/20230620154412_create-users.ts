48e3fa69c3b5335b10ca188e3b1274391950f9ea
Rilson Oliveira <rilson2.0@gmail.com>
2023-06-20 14:23:03 -0300

    feat: Create users table migration.

---
+++ db/migrations/20230620154412_create-users.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('users')
+}
