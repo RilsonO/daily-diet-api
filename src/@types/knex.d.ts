90e7b3f47ae897775c5f96aac3e4a3cf72a40030
Rilson Oliveira <rilson2.0@gmail.com>
2023-06-20 18:48:29 -0300

    feat: Add type definition for user table in knex.

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      name: string
+      email: string
+      password: string
+      created_at: string
+    }
+  }
+}
