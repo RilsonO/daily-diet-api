4f30454a5a7d5111be4b1c2429090a4693984e00
Rilson Oliveira <rilson2.0@gmail.com>
2023-06-20 14:24:18 -0300

    fix: Create created-at-to-meals migration.

---
+++ db/migrations/20230620162946_add-created-at-to-meals.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table('meals', (table) => {
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table('meals', (table) => {
+    table.dropColumn('created_at')
+  })
+}
